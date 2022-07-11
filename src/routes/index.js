8bc929905a4105af45dfcd3cac01d5529bf365c2
prasetyoow <praztamer@gmail.com>
2022-07-11 08:28:01 +0700

    chore: add auth route

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ router.use('/users', require('./users'));
 router.use('/profile', require('./profile'));
 router.use('/transactions', require('./transactions'));
 router.use('/transaction_type', require('./transaction_type'));
+router.use('/auth', require('./auth'));
 
 
 module.exports = router;
